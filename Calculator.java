077f624a504cd24d6eca11f09243aa51a02e1d14
jluqueco <jluqueco@gmail.com>
2022-02-18 23:07:29 -0500

    Final Version, added signs as operators and added a List to store all the operations keyed in

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,20 @@
 package calculator;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Calculator {
-	private final static int ADDITION = 1;
-	private final static int SUBSTRACCION = 2;
-	private final static int MULTIPLICATION = 3;
-	private final static int DIVISION = 4;
+	private final static String ADDITION = "1";
+	private final static String SUBSTRACCION = "2";
+	private final static String MULTIPLICATION = "3";
+	private final static String DIVISION = "4";
+	private final static String ADDITIONSIGN = "+";
+	private final static String SUBSTRACCIONSIGN = "-";
+	private final static String MULTIPLICATIONSIGN = "*";
+	private final static String DIVISIONSIGN = "/";
+	
 	private double result;
+	private List<String> list;
 	
 	/**
 	 * @return the result
@@ -16,20 +25,22 @@ public class Calculator {
 
 	public Calculator(double result) {
 		this.result = result;
+		this.list = new ArrayList<>();
+		list.add(String.valueOf(result));
 	}
 	
-	public void operation(double op1, int type) throws ArithmeticException {
+	public void operation(double op1, String type) throws ArithmeticException {
 		switch(type) {
-			case ADDITION: 
+			case ADDITION: case ADDITIONSIGN: 
 				result += op1;
 				break;
-			case SUBSTRACCION:
+			case SUBSTRACCION: case SUBSTRACCIONSIGN:
 				result -= op1;
 				break;
-			case MULTIPLICATION:
+			case MULTIPLICATION: case MULTIPLICATIONSIGN:
 				result *= op1;
 				break;
-			case DIVISION:
+			case DIVISION: case DIVISIONSIGN:
 				if(op1 != 0) {
 					result /= op1;
 				}else {
@@ -37,10 +48,19 @@ public class Calculator {
 				}
 				break;
 		}
+		
+		list.add(type);
+		list.add(String.valueOf(op1));
 	}
 
 	@Override
 	public String toString() {
-		return "\nCurrent result: " + result;
+		StringBuilder sb = new StringBuilder();
+		
+		for(String i : list) {
+			sb.append(i).append(" ");
+		}
+		
+		return "\nCurrent result of: " + sb.toString() + "= " + result;
 	}
 }
--- CalculatorMain.java
+++ CalculatorMain.java
@@ -1,36 +1,54 @@
 package calculator;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CalculatorMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		Calculator calc = null;
-		int option = 0;
+	public static boolean validate(String op) {
+		String options[] = {"1","2","3","4","5","/","*","-","+"};
 		
-		System.out.println("\n******Welcome to your CALCULATOR******\n");
-		
-		System.out.print("Please enter the initial value: ");
-		calc = new Calculator(sc.nextDouble());
-		
-		while (option != 5){
-			System.out.println("\nPlease choose the operation that you would like to perform: ");
-			System.out.println("\t1. ADDITION\n\t2. SUBSTRACTION\n\t3. MULTIPLICATION\n\t4. DIVISION\n\t5. EXIT");
-			option = sc.nextInt();
-			
-			if(option < 1 || option > 5) {
-				System.out.println("Not a valid entry, please try again.");	
-			}else if (option == 5) {
-				System.out.println("\nFinal Result: " + calc.getResult());
-				System.out.println("\nThank you for choosing CALCULATOR!...");
-			}else {
-				System.out.println("\nplease enter the value you wish operate");
-				calc.operation(sc.nextDouble(), option);
-				System.out.println(calc.toString());
+		for(String i : options) {
+			if(op.equals(i)) {
+				return true;	
 			}
+		}
 		
+		return false;
+		
+	}
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			Calculator calc = null;
+			String option = "";
+			
+			System.out.println("\n******Welcome to your CALCULATOR******\n");
+			
+			System.out.print("Please enter the initial value: ");
+			calc = new Calculator(sc.nextDouble());
+			
+			while (!option.equals("5")){
+				System.out.println("\nPlease choose the operation that you would like to perform: ");
+				System.out.println("\t1. ADDITION(+)\n\t2. SUBSTRACTION(-)\n\t3. MULTIPLICATION(*)\n\t4. DIVISION(/)\n\t5. EXIT");
+				option = sc.next();
+				
+				if(!validate(option)) {
+					System.out.println("Not a valid entry, please try again.");	
+				}else if (option.equals("5")) {
+					System.out.println("\nFinal Result: " + calc.getResult());
+					System.out.println("\nThank you for choosing CALCULATOR!...");
+				}else {
+					System.out.println("\nplease enter the value you wish operate");
+					calc.operation(sc.nextDouble(), option);
+					System.out.println(calc.toString());
+				}
+			
+			}
+		} catch (ArithmeticException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch(InputMismatchException e) {
+			System.err.println("\n*****Ilegal entry, please make sure that your input is a valid one*****");
 		}
 	}
 
